fafcde8ec235ddb2910040f17fb6220960ad189b
Himanshu474 <himanshurawat474@gmail.com>
2024-05-15 01:33:36 +0530

    adding navbar and cart

---
+++ React-Redux/src/components/cart/cart.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { removeFromCart } from '../../Redux/slices/cartSlice'
+import { useDispatch } from 'react-redux'
+
+const cart = ({cart}) => {
+  const dispatch=useDispatch()
+  return (
+    <div className='flex items-center justify-between'>
+        <img
+        className='w-[120px] h-[120px] object-contain my-5'
+        src={cart?.image}
+        />
+        <div className='w-[476px]'>
+            <div className="text-xl font-bold">{cart?.title}</div>
+        </div>
+        <div className="text-xl font-2xl">{cart?.price} TL</div>
+        <div className="text-xl font-xl">{cart?.quantity}</div>
+        <div onClick={()=>dispatch(removeFromCart(cart?.id))}
+        className='cursor-pointer bg-red-500 roundec-xl text-white w-[100px] h-4
+        flex items-center justify-center'
+        >
+          Delete Product
+        </div>
+    </div>
+  )
+}
+
+export default cart
