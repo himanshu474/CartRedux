35df0b8ff55fbb6dc87584386e080abbdf927b20
Himanshu474 <himanshurawat474@gmail.com>
2024-05-14 23:16:56 +0530

    adding redux persist

--- React-Redux/src/Redux/Store.jsx
+++ React-Redux/src/Redux/Store.jsx
@@ -1,5 +1,39 @@
 import {configureStore} from '@reduxjs/toolkit'
+import productSlice from './slices/productSlice'
+import cartSlice from './slices/cartSlice';
+import categorySlice from './slices/categorySlice'
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from "redux-persist";
+  import storage from "redux-persist/lib/storage"
+
+const persistConfig={
+    key:"root",
+    version:1,
+    storage,
+}
+
+const persistedCartReducer = persistReducer(persistConfig, cartSlice);
 
 export const store=configureStore({
-    reducer:{},
+    reducer:{
+        categories:categorySlice,
+        products:productSlice,
+        carts:persistedCartReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        }),
 })
+
+export let persistor = persistStore(store);
